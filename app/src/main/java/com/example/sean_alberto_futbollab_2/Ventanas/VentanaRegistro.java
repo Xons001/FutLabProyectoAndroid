be2da94fa4ccd4daab79c06f08f46933cf00efda
Xons001 <sean.saez.f@gmail.com>
2020-05-08 18:04:08 +0200

    Ventana Registro e iniciar sesion hecha

--- app/src/main/java/com/example/sean_alberto_futbollab_2/Ventanas/VentanaRegistro.java
+++ app/src/main/java/com/example/sean_alberto_futbollab_2/Ventanas/VentanaRegistro.java
@@ -3,6 +3,7 @@ package com.example.sean_alberto_futbollab_2.Ventanas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,13 +25,14 @@ public class VentanaRegistro extends AppCompatActivity {
     EditText nombreRegistro, apellidoRegistro, mailRegistro, passwordRegistro;
 
     String urlStr;
+    String nombre, apellidos, mail, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ventana_registro);
 
-        btnRegistrar = findViewById(R.id.button_registrarse);
+        btnRegistrar = findViewById(R.id.boton_confirmar_registro);
         btnVolver = findViewById(R.id.boton_volver_atras);
 
         nombreRegistro = findViewById(R.id.nombre_registro);
@@ -46,66 +48,70 @@ public class VentanaRegistro extends AppCompatActivity {
             }
         });
 
-        /*btnRegistrar.setOnClickListener(new View.OnClickListener() {
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nombre = nombreRegistro.getText().toString();
-                String apellidos = apellidoRegistro.getText().toString();
-                String mail = mailRegistro.getText().toString();
-                String pass = passwordRegistro.getText().toString();
-                urlStr = getURL_Registrar(nombre, apellidos, mail, pass);
+                nombre = nombreRegistro.getText().toString();
+                apellidos = apellidoRegistro.getText().toString();
+                mail =  mailRegistro.getText().toString();
+                pass = passwordRegistro.getText().toString();
 
-                sendDatosClientes();
-            }
-        });*/
-    }
-
-    public static String getURL_Registrar(String nombre, String apellidos, String mail, String password) {
-        String url = "https://futlab-credito-sintesis.herokuapp.com/registar/" + nombre + "/" + apellidos + "/" + mail + "/" + password;
-        return url;
-    }
-    public void sendDatosClientes() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL(urlStr);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-                    conn.setRequestProperty("Accept", "application/json");
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
-
-                    JSONObject jsonParam = new JSONObject();
-                    jsonParam.put("nombre", nombreRegistro.getText().toString());
-                    jsonParam.put("apellidos", apellidoRegistro.getText().toString());
-                    jsonParam.put("mail", mailRegistro.getText().toString());
-                    jsonParam.put("password", passwordRegistro.getText().toString());
-
-                    Log.i("JSON", jsonParam.toString());
-                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                    os.writeBytes(jsonParam.toString());
-
-                    os.flush();
-                    os.close();
-
-                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
-                    Log.i("MSG", conn.getResponseMessage());
-
-                    Intent intentLogin = new Intent(getApplicationContext(), Login.class);
-                    startActivity(intentLogin);
-
-                    Toast.makeText(VentanaRegistro.this, "Cliente registrado", Toast.LENGTH_SHORT).show();
-
-                    conn.disconnect();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Toast.makeText(VentanaRegistro.this, "El cliente no se has podido registrar", Toast.LENGTH_SHORT).show();
-                }
+                new insertarRegistro().execute();
+                //sendDatosClientes();
             }
         });
+    }
 
-        thread.start();
+    private class insertarRegistro extends AsyncTask<Void, Void, Void> {
+        boolean insertado = false;
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                URL url = new URL("https://futlab-credito-sintesis.herokuapp.com/registrar");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("nombre", nombre);
+                jsonParam.put("apellidos", apellidos);
+                jsonParam.put("mail", mail);
+                jsonParam.put("password", pass);
+
+                Log.i("JSON", jsonParam.toString());
+                DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                os.writeBytes(jsonParam.toString());
+
+                os.flush();
+                os.close();
+
+                Log.i("STATUS prueba", String.valueOf(conn.getResponseCode()));
+                Log.i("MSG supremo", conn.getResponseMessage());
+
+                insertado = true;
+                //Toast.makeText(VentanaRegistro.this, "Cliente registrado", Toast.LENGTH_SHORT).show();
+
+                conn.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(VentanaRegistro.this, "El cliente no se has podido registrar", Toast.LENGTH_SHORT).show();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if(insertado == true){
+                Toast.makeText(VentanaRegistro.this, "Cliente registrado", Toast.LENGTH_SHORT).show();
+                Intent intentLogin = new Intent(getApplicationContext(), Login.class);
+                startActivity(intentLogin);
+            } else {
+                Toast.makeText(VentanaRegistro.this, "El cliente no se has podido registrar", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
