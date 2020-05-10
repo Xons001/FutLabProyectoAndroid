4567a68fb7d023d4de3fc77302235dec1e2de141
Xons001 <sean.saez.f@gmail.com>
2020-05-10 19:12:54 +0200

    Ventana Cliente visualmente acabado

---
+++ app/src/main/java/com/example/sean_alberto_futbollab_2/Frames/CursosDisponibles.java
@@ -0,0 +1,24 @@
+package com.example.sean_alberto_futbollab_2.Frames;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sean_alberto_futbollab_2.R;
+
+public class CursosDisponibles extends Fragment {
+
+    public CursosDisponibles() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.ventana_cliente, container, false);
+    }
+}
--- app/src/main/java/com/example/sean_alberto_futbollab_2/Ventanas/VentanaCliente.java
+++ app/src/main/java/com/example/sean_alberto_futbollab_2/Ventanas/VentanaCliente.java
@@ -1,16 +1,45 @@
 package com.example.sean_alberto_futbollab_2.Ventanas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.example.sean_alberto_futbollab_2.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class VentanaCliente extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ventana_cliente);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager= findViewById(R.id.viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
     }
+
+
 }
